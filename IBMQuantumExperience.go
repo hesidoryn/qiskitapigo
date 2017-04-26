af02be4570b63542ee55c6accfbe001c251b382f
heorhi <heorhi.sidoryn@gmail.com>
2017-04-26 20:10:18 +0300

    Auth

--- IBMQuantumExperience.go
+++ IBMQuantumExperience.go
@@ -1,49 +1,67 @@
 package qiskitapigo
 
-//import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
 
-// const CONFIG_BASE = {
-//   URL: "https://quantumexperience.ng.bluemix.net/api"
-// }
-
-type Api struct{}
+const URL = "https://quantumexperience.ng.bluemix.net/api"
 
 type Credentials struct {
-	ID string `json:"id"`
-	TTL int `json:"ttl"`
-	Created time.Time `json:"created"`
-	UserID string `json:"userId"`
+	ID       string    `json:"id"`
+	TTL      int       `json:"ttl"`
+	Created  time.Time `json:"created"`
+	UserID   string    `json:"userId"`
+	ApiToken string
 }
 
-func IBMQuantumExperience() interface{} {
-    //resp, _ := http.Post(CONFIG_BASE.URL + "/users/loginWithToken", "application/json", &buf)
-    return Api{}
+type Api struct {
+	Creds Credentials
 }
 
-func (api *Api) Get_Code() interface{} {
-    return 1
+func IBMQuantumExperience(token string) Api {
+	creds := Credentials{}
+	creds.ApiToken = token
+
+	p := map[string]string{"apiToken": token}
+	b := new(bytes.Buffer)
+	json.NewEncoder(b).Encode(p)
+	resp, err := http.Post(URL+"/users/loginWithToken", "application/json", b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewDecoder(resp.Body).Decode(&creds)
+	return Api{creds}
+}
+
+func (api *Api) Get_Code() int {
+	return 1
 }
 
 func (api *Api) Get_Last_Codes() interface{} {
-    return 2
+	return 2
 }
 
 func (api *Api) Get_Execution() interface{} {
-    return 3
+	return 3
 }
 
 func (api *Api) Get_Result_From_Execution() interface{} {
-    return 4
+	return 4
 }
 
 func (api *Api) Run_Experiment() interface{} {
-    return 5
+	return 5
 }
 
 func (api *Api) Run_Jobes() interface{} {
-    return 6
+	return 6
 }
 
 func (api *Api) Get_Job() interface{} {
-    return 7
+	return 7
 }
